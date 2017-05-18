084c8a6c1283e3a28d7793f457f082d7770e6d78
Ariane Deschenes Foucher <ariane.ariane@telus.com>
2017-05-18 14:17:05 -0400

    set up rails app with an index page and an instance of a standard REST resource, called email

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'index/index'
+
+  resources :email
+  
+  root 'index#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
