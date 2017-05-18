543132595408f29c70f57179a1f31a2d8462e0c7
Ariane Deschenes Foucher <ariane.ariane@telus.com>
2017-05-18 14:25:53 -0400

    generated an email controller and basic view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'index/index'
 
-  resources :email
-  
+  resources :emails
+
   root 'index#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
