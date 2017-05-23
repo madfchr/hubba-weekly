5e2d91248850803c14ae799fc552184c96a04bd7
Ariane Deschenes Foucher <ariane.ariane@telus.com>
2017-05-23 14:13:48 -0400

    added tag column to sections to better sort the two kinds, industries and common

---
+++ db/migrate/20170523181112_add_tag_to_sections.rb
@@ -0,0 +1,5 @@
+class AddTagToSections < ActiveRecord::Migration[5.1]
+  def change
+    add_column :sections, :tag, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523170745) do
+ActiveRecord::Schema.define(version: 20170523181112) do
 
   create_table "emails", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170523170745) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "tag"
   end
 
   create_table "stories", force: :cascade do |t|
