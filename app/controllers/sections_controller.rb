8e40c9ebb99c67d6ebc739c44107325f26c9b9eb
Ariane Deschenes Foucher <ariane.ariane@telus.com>
2017-05-24 18:44:11 -0400

    EIGHT HOURS// created an index and show for sections

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -4,6 +4,45 @@ class SectionsController < ApplicationController
       @sections = Section.all
     end
 
+    def show
+      @section = Section.find(params[:id])
+    end
+
+    def new
+      @section = Section.new
+    end
+
+    def edit
+      @section = Section.find(params[:id])
+    end
+
+    def create
+      @section = Section.new(section_params)
+
+      if @section.save
+        redirect_to @section
+      else
+        render 'new'
+      end
+    end
+
+    def update
+      @section = Section.find(params[:id])
+
+      if @section.update(section_params)
+        redirect_to @section
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+      @section = Section.find(params[:id])
+      @section.destroy
+
+      redirect_to sectioni_path
+    end
+
     private
     def section_params
       params.require(:section).permit(:name, :tag)
